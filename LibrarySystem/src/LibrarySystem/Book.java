e72d9aa8c8e9941ca7794dc12b201aa7eb582783
gurdjieff <ajie@ajies-MacBook-Pro.local>
2016-03-01 20:50:11 -0600

    daiyu zhang

--- LibrarySystem/src/LibrarySystem/Book.java
+++ LibrarySystem/src/LibrarySystem/Book.java
@@ -21,17 +21,27 @@ public class Book implements Serializable {
 	public void addCopy(BookCopy bookCopy) {
 		bookCopies.add(bookCopy);
 	}
+	
+	public void addAuthor(Author author) {
+		authors.add(author);
+	}
 
 	public Book() {
 		bookCopies = new LinkedList<BookCopy>();
 		authors = new LinkedList<Author>();
 	}
 
-	public Book(String ISBN, String title, int maxCheckoutLength) {
+	public Book(String ISBN, String title, int maxCheckoutLength,
+			int numOfCopies,List<Author>authors) {
 		this();
 		this.ISBN = ISBN;
 		this.title = title;
 		this.maxCheckoutLength = maxCheckoutLength;
+		
+		for (int i = 0; i < numOfCopies; i++) {
+			addCopy(new BookCopy(i, true, this));
+		}
+		authors.addAll(authors);
 	}
 
 }
