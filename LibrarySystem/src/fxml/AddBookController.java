4493447a4e650d1e13c39e17ba0d13dcc149f677
gurdjieff <ajie@ajies-MacBook-Pro.local>
2016-03-01 21:27:09 -0600

    daiyu zhang

--- LibrarySystem/src/fxml/AddBookController.java
+++ LibrarySystem/src/fxml/AddBookController.java
@@ -6,11 +6,21 @@
 package fxml;
 
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import LibrarySystem.Address;
+import LibrarySystem.Author;
+import LibrarySystem.Book;
+import LibrarySystem.LibraryMember;
+import LibrarySystem.OperationAssistant;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
 /**
@@ -37,7 +47,8 @@ public class AddBookController implements Initializable {
     @FXML private TextField author2Last;
     @FXML private TextField maxLength;
     @FXML private TextField numberCopies;
-    
+    @FXML private javafx.scene.control.Button addBookCop ;
+
     
     // button Add click()
     public void addButtonClick(ActionEvent event){
@@ -53,6 +64,48 @@ public class AddBookController implements Initializable {
         System.out.println(author2Last.getText());
         System.out.println(maxLength.getText());
         System.out.println(numberCopies.getText());
+        
+        if (bookISBN.getText().length() == 0
+        			|| bookTitle.getText().length() == 0
+					|| author1Name.getText().length() == 0
+					|| author1Last.getText().length() == 0
+					|| author2Name.getText().length() == 0
+					|| author2Last.getText().length() == 0
+					|| maxLength.getText().length() == 0
+					|| numberCopies.getText().length() == 0) {
+            Alert errorMessage = new Alert(Alert.AlertType.ERROR, "Please complete all of the information!");
+            errorMessage.showAndWait();
+            return;
+        	
+		}
+        
+        int length = 0;
+        int copies = 0;
+        
+        try {
+        	length = Integer.parseInt(maxLength.getText());
+        	copies = Integer.parseInt(numberCopies.getText());
+		} catch (Exception e) {
+			Alert errorMessage = new Alert(Alert.AlertType.ERROR, "MaxLength or number of copies is wrong!");
+            errorMessage.showAndWait();
+            return;
+			// TODO: handle exception
+		}
+        
+        List<Author> authors = new LinkedList<Author>(){
+        	{
+        		add(new Author(author1Name.getText(), author1Last.getText()));
+        		add(new Author(author2Name.getText(), author2Last.getText()));
+
+        	}
+        };
+        OperationAssistant.addBook(new Book(bookISBN.getText(), 
+        		bookTitle.getText(), length, copies, authors));
+   
+       // closing itself
+//		Stage stage = (Stage) addButton.getScene().getWindow();
+//		stage.close();
+        
         
         
     }
