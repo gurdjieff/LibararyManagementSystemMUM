24f9f796f0f37cd6dc85618de90385de14cbb721
jianguoguo <jianguo.java@gmail.com>
2016-03-03 11:26:12 +0800

    update

--- LibrarySystem/src/LibrarySystem/db/DataAccessFacede.java
+++ LibrarySystem/src/LibrarySystem/db/DataAccessFacede.java
@@ -1,4 +1,5 @@
 package LibrarySystem.db;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,78 +9,78 @@ import LibrarySystem.Book;
 import LibrarySystem.CheckoutRecord;
 import LibrarySystem.CheckoutRecordEntry;
 import LibrarySystem.ConstTypes;
+import LibrarySystem.ConstTypes.UserType;
 import LibrarySystem.LibraryMember;
 import LibrarySystem.User;
-import LibrarySystem.ConstTypes.UserType;
 
 public class DataAccessFacede {
-	
-	static public void init () {
+
+	public static void init() {
 		if (getAllBooks().size() == 0) {
 			for (int i = 0; i < 6; i++) {
 				List<Author> authors = new LinkedList<Author>();
-				authors.add(new Author("1firstName1"+i, "1lastName"+i));
-				authors.add(new Author("1firstName1"+i, "1lastName"+i));
-				addBook(new Book(""+(i+1), "book"+(i+1), i+1, i+1, authors));
+				authors.add(new Author("1firstName1" + i, "1lastName" + i));
+				authors.add(new Author("1firstName1" + i, "1lastName" + i));
+				addBook(new Book("" + (i + 1), "book" + (i + 1), i + 1, i + 1, authors));
 			}
 		}
-		
+
 		if (getAllMembers().size() == 0) {
 			addMember(new LibraryMember("firstname01", "lastName02", null));
 			addMember(new LibraryMember("firstname01", "lastName02", null));
 			addMember(new LibraryMember("firstname01", "lastName02", null));
 			addMember(new LibraryMember("firstname01", "lastName02", null));
 		}
-		
+
 		if (getAllUsers().size() == 0) {
 			for (int i = 0; i < 4; i++) {
 				addUser(new User(UserType.LIBRARIAN, "l" + i, "1"));
 				addUser(new User(UserType.ADMINE, "a" + i, "1"));
 				addUser(new User(UserType.BOTH, "b" + i, "1"));
 			}
-		}	
+		}
 	}
-	
-	static public void clear () {
-		Tool.save(ConstTypes.LIBRARYMEMBERSTR , null);
-		Tool.save(ConstTypes.BOOKSTR , null);
-		Tool.save(ConstTypes.USERSTR , null);
+
+	public static void clear() {
+		Tool.save(ConstTypes.LIBRARYMEMBERSTR, null);
+		Tool.save(ConstTypes.BOOKSTR, null);
+		Tool.save(ConstTypes.USERSTR, null);
 	}
+
 	public static void main(String[] args) {
 		clear();
 		init();
 	}
-	
-	public static void getAllBook () {
+
+	public static void getAllBook() {
 		List<Book> books = getAllBooks();
 		for (int i = 0; i < books.size(); i++) {
 			System.out.println(books.get(i).toString());
 		}
 	}
-	
-	static public List<LibraryMember> getAllMembers () {
+
+	@SuppressWarnings("unchecked")
+	public static List<LibraryMember> getAllMembers() {
 		Object obj = Tool.read(ConstTypes.LIBRARYMEMBERSTR);
-		if (obj == null) {			
+		if (obj == null) {
 			return new ArrayList<LibraryMember>();
-		}  
+		}
 		return (List<LibraryMember>) obj;
 	}
-	
-	static public void membersPersistence (List<LibraryMember> members) {
-		Tool.save(ConstTypes.LIBRARYMEMBERSTR , members);
-	}
-	
-	static public void booksPersistence (List<Book> books) {
-		Tool.save(ConstTypes.BOOKSTR , books);
-	}
-	
-	static public void usersPersistence (List<User> users) {
-		Tool.save(ConstTypes.USERSTR , users);
 
-		
+	public static void membersPersistence(List<LibraryMember> members) {
+		Tool.save(ConstTypes.LIBRARYMEMBERSTR, members);
 	}
-	
-	static public LibraryMember getMemberWithId (int ID) {
+
+	public static void booksPersistence(List<Book> books) {
+		Tool.save(ConstTypes.BOOKSTR, books);
+	}
+
+	public static void usersPersistence(List<User> users) {
+		Tool.save(ConstTypes.USERSTR, users);
+	}
+
+	public static LibraryMember getMemberWithId(int ID) {
 		List<LibraryMember> members = getAllMembers();
 		for (int i = 0; i < members.size(); i++) {
 			if (members.get(i).getID() == ID) {
@@ -88,17 +89,17 @@ public class DataAccessFacede {
 		}
 		return null;
 	}
-	
-	static public LibraryMember addMember (LibraryMember member) {
+
+	public static LibraryMember addMember(LibraryMember member) {
 		List<LibraryMember> members = getAllMembers();
 		member.setID(members.size());
 		members.add(member);
 		membersPersistence(members);
-//		save
+		// save
 		return member;
 	}
-	
-	static public LibraryMember updateMember (LibraryMember member) {
+
+	public static LibraryMember updateMember(LibraryMember member) {
 		List<LibraryMember> members = getAllMembers();
 		for (int i = 0; i < members.size(); i++) {
 			if (members.get(i).getID() == member.getID()) {
@@ -109,59 +110,58 @@ public class DataAccessFacede {
 		}
 		return null;
 	}
-	
-	static public List<Book> getAllBooks () {
+
+	@SuppressWarnings("unchecked")
+	public static List<Book> getAllBooks() {
 		Object obj = Tool.read(ConstTypes.BOOKSTR);
-		if (obj == null) {			
+		if (obj == null) {
 			return new ArrayList<Book>();
-		}  
+		}
 		return (List<Book>) obj;
 	}
-	
-	static public Book getBook(String ISBN) {
+
+	public static Book getBook(String ISBN) {
 		List<Book> books = getAllBooks();
 		for (int i = 0; i < books.size(); i++) {
 			if (books.get(i).getISBN().equals(ISBN)) {
 				return books.get(i);
 			}
-			
+
 		}
 		return null;
 	}
-	
-	static public Book updateBook (Book book) {
+
+	public static Book updateBook(Book book) {
 		List<Book> books = getAllBooks();
 		for (int i = 0; i < books.size(); i++) {
 			if (books.get(i).getISBN().equals(book.getISBN())) {
 				books.set(i, book);
 				booksPersistence(books);
-//				save
+				// save
 				return book;
 			}
-		}		
-		
+		}
+
 		return book;
 	}
-	
-	static public Book addBook (Book book) {
-		List<Book> books = getAllBooks();	
+
+	public static Book addBook(Book book) {
+		List<Book> books = getAllBooks();
 		books.add(book);
 		booksPersistence(books);
-//		save
-		
 		return book;
 	}
-	
-	static public List<User> getAllUsers () {
+
+	@SuppressWarnings("unchecked")
+	public static List<User> getAllUsers() {
 		Object obj = Tool.read(ConstTypes.USERSTR);
-		if (obj == null) {			
+		if (obj == null) {
 			return new ArrayList<User>();
-		}  
+		}
 		return (List<User>) obj;
 	}
-	
-	
-	static public User getUser (String ID, String password) {
+
+	public static User getUser(String ID, String password) {
 		List<User> users = getAllUsers();
 		for (int i = 0; i < users.size(); i++) {
 			if (users.get(i).getID().equals(ID) && users.get(i).getPassword().equals(password)) {
@@ -170,36 +170,34 @@ public class DataAccessFacede {
 		}
 		return null;
 	}
-	
-	static public User addUser (User user) {
+
+	public static User addUser(User user) {
 		List<User> users = getAllUsers();
 		users.add(user);
 		usersPersistence(users);
 		return user;
 	}
-        
-        public static void printUserCheckoutRecords(int id) {
-            LibraryMember member = getMemberWithId(id);
-            String line = new String(new char[20]).replace("\0", "-");
-            System.out.println(line);
-            System.out.printf("--%s %s(%s) checkout record list--\r\n",
-                    member.getFirstName(),
-                    member.getLastName(),
-                    member.getID());
-            System.out.printf("|%20s|", "Book");
-            System.out.printf("%15s|", "CheckoutDate");
-            System.out.printf("%15s|\n", "DueDate");
-            System.out.println(line);
 
-            CheckoutRecord checkoutRecord = member.getCheckoutRecord();
+	public static void printUserCheckoutRecords(int id) {
+		LibraryMember member = getMemberWithId(id);
+		String line = new String(new char[20]).replace("\0", "-");
+		System.out.println(line);
+		System.out.printf("--%s %s(%s) checkout record list--\r\n", member.getFirstName(), member.getLastName(),
+				member.getID());
+		System.out.printf("|%20s|", "Book");
+		System.out.printf("%15s|", "CheckoutDate");
+		System.out.printf("%15s|\n", "DueDate");
+		System.out.println(line);
 
-            List<CheckoutRecordEntry> entries = checkoutRecord.getCheckoutRecordEntries();
+		CheckoutRecord checkoutRecord = member.getCheckoutRecord();
 
-            entries.stream().forEach((entry)->{
-                System.out.printf("|%20s|", entry.getBookCopy().getBook().getTitle());
-                System.out.printf("%15s|", entry.getCheckoutDate());
-                System.out.printf("%15s|\n", entry.getDueDate());
-                System.out.println(line);
-            });
-    }
+		List<CheckoutRecordEntry> entries = checkoutRecord.getCheckoutRecordEntries();
+
+		entries.stream().forEach((entry) -> {
+			System.out.printf("|%20s|", entry.getBookCopy().getBook().getTitle());
+			System.out.printf("%15s|", entry.getCheckoutDate());
+			System.out.printf("%15s|\n", entry.getDueDate());
+			System.out.println(line);
+		});
+	}
 }
--- LibrarySystem/src/LibrarySystem/db/Tool.java
+++ LibrarySystem/src/LibrarySystem/db/Tool.java
@@ -1,73 +1,48 @@
 package LibrarySystem.db;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.LinkedList;
-import java.util.List;
-
-import LibrarySystem.Address;
-import LibrarySystem.Book;
-import LibrarySystem.ConstTypes;
-import LibrarySystem.ConstTypes.UserType;
-import LibrarySystem.Person;
-import LibrarySystem.User;
+import java.util.Properties;
 
 public class Tool {
 
-//	public static final String OUTPUT_DIR = System.getProperty("user.dir")
-//			+ "\\src\\LibrarySystem\\librarysample\\dataaccess\\storage";
-
-	public static final String OUTPUT_DIR = "/Users/ajie/dataBase";
-
-	private static int AUTOID = 0;
-
-	public static int nextID() {
-		AUTOID++;
-		return AUTOID;
-	}
-
+	private static String OUTPUT_DIR = "";
 	static {
-		init();
-	}
-
-	public static void init() {
-		for (int i = 0; i < 10; i++) {
-			save(ConstTypes.BOOKSTR + i, new Book(i + "", i + "", i));
-			nextID();
-		}
-
-		for (int j = 0; j < 10; j++) {
-			Person person = new Person();
-			person.setFirstName("firstname" + j);
-			person.setLastName("lastname" + j);
-			Address address = new Address();
-			address.setCity("IA" + j);
-			person.setAddress(address);
-			save(ConstTypes.PERSONSTR + j, person);
-		}
-
-		// init user
-		initUsers();
-	}
-
-	private static void initUsers() {
-		List<User> users = new LinkedList<User>() {
-			private static final long serialVersionUID = 1L;
-
-			{
-				for (int i = 0; i < 10; i++) {
-					add(new User(UserType.LIBRARIAN, "l" + i, "1"));
-					add(new User(UserType.ADMINE, "a" + i, "1"));
-					add(new User(UserType.BOTH, "b" + i, "1"));
-					nextID();
-				}
+		Properties props = System.getProperties();
+		String osName = props.getProperty("os.name");
+		File file = null;
+		// for windows
+		if (osName.indexOf("Windows") != -1) {
+			try {
+				OUTPUT_DIR = "c:\\LibrarySystemDB";
+				file = new File(OUTPUT_DIR);
+				file.mkdirs();
+			} catch (Exception e) {
+				OUTPUT_DIR = "d:\\LibrarySystemDB";
+				file = new File(OUTPUT_DIR);
+				file.mkdirs();
 			}
-		};
-		save(ConstTypes.USERSTR, users);
+		} else {
+			// for mac or other
+			try {
+				OUTPUT_DIR = "/Users/LibrarySystemDB";
+				file = new File(OUTPUT_DIR);
+				file.mkdirs();
+			} catch (Exception e) {
+				OUTPUT_DIR = "/Users/tmp/LibrarySystemDB";
+				file = new File(OUTPUT_DIR);
+				file.mkdirs();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Tool.read("");
 	}
 
 	public static void save(String name, Object member) {
@@ -88,32 +63,6 @@ public class Tool {
 		}
 	}
 
-	public static void save2(String name, Object member) {
-		ObjectOutputStream out = null;
-		try {
-			Path path = FileSystems.getDefault().getPath(OUTPUT_DIR + "\\" + member.getClass().getSimpleName(), name);
-			out = new ObjectOutputStream(Files.newOutputStream(path));
-			out.writeObject(member);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (out != null) {
-				try {
-					out.close();
-				} catch (Exception e) {
-				}
-			}
-		}
-	}
-
-	public static boolean ifExist(String name) {
-		return read(name) == null ? false : true;
-	}
-
-	public static void update(String name, Object member) {
-		save(name, member);
-	}
-
 	public static Object read(String name) {
 		ObjectInputStream in = null;
 		Object member = null;
@@ -122,7 +71,7 @@ public class Tool {
 			in = new ObjectInputStream(Files.newInputStream(path));
 			member = in.readObject();
 		} catch (Exception e) {
-			//e.printStackTrace();
+			// e.printStackTrace();
 		} finally {
 			if (in != null) {
 				try {
@@ -133,24 +82,4 @@ public class Tool {
 		}
 		return member;
 	}
-
-	public static Object read(String name, Object member) {
-		ObjectInputStream in = null;
-		try {
-			Path path = FileSystems.getDefault().getPath(OUTPUT_DIR + "\\" + member.getClass().getSimpleName(), name);
-			in = new ObjectInputStream(Files.newInputStream(path));
-			member = in.readObject();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (in != null) {
-				try {
-					in.close();
-				} catch (Exception e) {
-				}
-			}
-		}
-		return member;
-	}
-
 }
